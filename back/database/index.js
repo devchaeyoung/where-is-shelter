13a573148f67b2eea551488d407f715292f18748
[AI8/1반] 진채영 <chaemaa00@gmail.com>
2023-08-16 16:57:08 +0900

    Docs: mongodb연결

--- back/database/index.js
+++ back/database/index.js
@@ -1,1 +1,14 @@
 //DB용
+
+// <username>, <password>를 바꾸어 줍니다.
+const ATLAS_URL_CORRECT =
+  "mongodb+srv://elice_3_team:1234@cluster0.orhh1yn.mongodb.net/";
+
+// mongoose 연결 - require 버전
+const mongoose = require("mongoose");
+
+// mongoose 연결 - import 버전
+import mongoose from "mongoose";
+
+mongoose.connect(ATLAS_URL_CORRECT);
+db.on("connected", () => console.log("정상적으로 연결되었습니다."));
