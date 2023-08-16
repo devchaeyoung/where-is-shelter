c7d58cc92ae428dae0bec83be2ed8eb7e4e3c349
[AI8/1반] 진채영 <chaemaa00@gmail.com>
2023-08-16 19:03:39 +0900

    test

--- back/index.js
+++ back/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
 
@@ -9,13 +11,18 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-모;
 
 const ATLAS_URL_CORRECT =
   "mongodb+srv://elice_3_team:1234@cluster0.orhh1yn.mongodb.net/";
 
 // mongoose 연결 - require 버전
-const mongoose = require("mongoose");
+const db = mongoose.connection;
 
 mongoose.connect(ATLAS_URL_CORRECT);
 db.on("connected", () => console.log("정상적으로 연결되었습니다."));
+
+db.on("error", (error) =>
+  console.error(
+    "MongoDB 연결에 실패하였습니다...\n" + ATLAS_URL_CORRECT + "\n" + error
+  )
+);
