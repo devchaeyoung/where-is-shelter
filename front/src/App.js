bc0dec72ef519a26de7066227cbe9b5aa2b423b9
[AI8/1반] 강현지 <newbie098766@gmail.com>
2023-08-18 15:35:40 +0900

    Navbar.js 수정

--- front/src/App.js
+++ front/src/App.js
@@ -4,24 +4,11 @@ import './index.css';
 import Header from "./components/Layout/Header";
 import Navbar from "./components/Layout/Navbar";
 
-import PoiPage from "./pages/PoiPage";
-import WeatherPage from "./pages/WeatherPage";
-import InfographicPage from "./pages/InfographicPage";
-import MyPage from "./pages/MyPage";
-import { Route, Routes, Link } from 'react-router-dom';
-
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
-
-      <Routes>
-        <Route path="/PoiPage" element={<PoiPage />}></Route>
-        <Route path="/WeatherPage" element={<WeatherPage />}></Route>
-        <Route path="/InfographicPage" element={<InfographicPage />}></Route>
-        <Route path="/Mypage" element={<MyPage />}></Route>
-      </Routes>
     </div>
   );
 }
--- front/src/components/Layout/Navbar.js
+++ front/src/components/Layout/Navbar.js
@@ -1,19 +1,27 @@
-import { React, useState, Link, Route, Routes } from 'react';
+import { React } from 'react';
+import { Link, Route, Routes} from 'react-router-dom';
+import PoiPage from "../../pages/PoiPage";
+import WeatherPage from "../../pages/WeatherPage";
+import InfographicPage from "../../pages/InfographicPage";
+import MyPage from "../../pages/MyPage";
+
 
 function Navbar () {
-    const [showPoi, setShowPoi] = useState()
     return (
         <div className="w-full h-20">
-            <Routes>
-                <Route path="../page/PoiPage" element={<PoiPage />}></Route>
-                <Route path="../page/WeatherPage" element={<WeatherPage />}></Route>
-                <Route path="../page/InfographicPage" element={<InfographicPage />}></Route>
-                <Route path="../page/Mypage" element={<MyPage />}></Route>
-            </Routes>
+
             <Link to="/PoiPage">극한날씨 쉼터</Link>
             <Link to="/WeatherPage">기상정보</Link>
             <Link to="/InfographicPage">인포그래픽</Link>
             <Link to="/Mypage">마이페이지</Link>
+
+            <Routes>
+                <Route path="/PoiPage" element={<PoiPage />}></Route>
+                <Route path="/WeatherPage" element={<WeatherPage />}></Route>
+                <Route path="/InfographicPage" element={<InfographicPage />}></Route>
+                <Route path="/Mypage" element={<MyPage />}></Route>
+            </Routes>
+
         </div>
     )
 }
