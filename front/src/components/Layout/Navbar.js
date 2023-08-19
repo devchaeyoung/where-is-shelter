ac607086048330f6d6877bb571d2304777888ef2
[AI8/1반] 김준섭 <elice.kdt@gmail.com>
2023-08-19 15:40:35 +0900

    front 브랜치 강제 최신화

--- front/src/components/Layout/Navbar.js
+++ front/src/components/Layout/Navbar.js
@@ -1,6 +1,8 @@
+import { React, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 
-import { React } from 'react';
+function menuStyleToggle () {
+}
 
 function Navbar() {
   return (
@@ -8,12 +10,12 @@ function Navbar() {
 
       <div className="flex flex-row w-full h-20 justify-between items-center px-8 font-bold text-lg">
         <div>
-          <Link to="/poi" className="mr-12 underline decoration-solid decoration-4 underline-offset-8">극한날씨 쉼터</Link>
-          <Link to="/weather" className="mr-12">기상정보</Link>
-          <Link to="/infographic" className="mr-12">인포그래픽</Link>
+          <Link to="/poi" id="1" className="mr-12">극한날씨 쉼터</Link>
+          <Link to="/weather" id="2" className="mr-12">기상정보</Link>
+          <Link to="/infographic" id="3" className="mr-12">인포그래픽</Link>
         </div>
         <div>
-          <Link to="/mypage">마이페이지</Link>
+          <Link to="/mypage" id="4">마이페이지</Link>
         </div>
       </div>
     </div>
