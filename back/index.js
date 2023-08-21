dc7d3c82eb42e2427321da2b8f2a25d4cf306e57
[AI8/1반] 진채영 <chaemaa00@gmail.com>
2023-08-21 19:46:18 +0900

    fix: cors 전체 허용

--- back/index.js
+++ back/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "./configs/dbConfig";
+import cors from "cors";
 import { shelterRouter } from "./routers/shelterRouter";
 
 dotenv.config();
@@ -8,7 +9,7 @@ db();
 const port = process.env.SERVER_PORT;
 
 const app = express();
-
+app.use(cors());
 app.use(shelterRouter);
 
 app.get("/", (req, res) => {
--- back/middlewares/corsMiddleware.js
+++ back/middlewares/corsMiddleware.js
@@ -7,18 +7,22 @@ const app = express();
 dotenv.config();
 db();
 const port = process.env.SERVER_PORT;
+const whitelist = {
+  origin: "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode",
+  optionsSucessStatus: 200,
+};
 
-app.use(express.json());
-app.use(cors());
+// function CorsMiddleware() {
+//   app.use(express.json());
+//   app.use(cors(whitelist));
+//   // CORS 에러시 호출
+//   app.get("/", function (req, res, next) {
+//     res.json({ msg: `This is CORS-enabled for all origins!` });
+//   });
 
-function CorsMiddleware() {
-  app.get("/", function (req, res, next) {
-    res.json({ msg: `This is CORS-enabled for all origins!` });
-  });
-
-  app.listen({ port }, function () {
-    console.log(`CORS-enabled web server listening on port ${port}`);
-  });
-}
+//   app.listen({ port }, function () {
+//     console.log(`CORS-enabled web server listening on port ${port}`);
+//   });
+// }
 
 export default CorsMiddleware;
