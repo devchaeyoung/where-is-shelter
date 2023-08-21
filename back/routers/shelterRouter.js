76643b1a85b00778b415c0992b11e0b0ef0e8a37
[AI8/1반] 진채영 <chaemaa00@gmail.com>
2023-08-21 12:18:55 +0900

    fix러부분 주석처리

--- back/routers/shelterRouter.js
+++ back/routers/shelterRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import ShelterController from "../controllers/shelterController";
 
+const sheltersRouter = Router();
 const shelterRouter = Router();
+sheltersRouter.get("/home", ShelterController.getShelters);
+shelterRouter.get("/home/:shelter_id", ShelterController.getShelter);
 
-shelterRouter.get("/home", ShelterController.getShelters);
-
-export { shelterRouter };
+export { sheltersRouter, shelterRouter };
