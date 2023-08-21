6bedfbaa1aa994d622dc382f4534da3e6b84e2af
[AI8/1반] 진채영 <chaemaa00@gmail.com>
2023-08-21 22:34:05 +0900

    Docs: mongoURL 재설정

--- back/configs/dbConfig.js
+++ back/configs/dbConfig.js
@@ -1,24 +1,25 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-const ATLAS_URL_CORRECT =
-  "mongodb+srv://elice_3_team:1234@cluster0.orhh1yn.mongodb.net/shelter";
+dotenv.config();
+const mongoURL = process.env.MONGO_URL;
 
 function databaseConnection() {
   // mongoose 연결 - require 버전
-  const db = mongoose.connection;
 
-  mongoose.connect(ATLAS_URL_CORRECT);
-  db.on("connected", () => console.log("정상적으로 연결되었습니다."));
+  const db = mongoose.connection;
+  mongoose.connect(mongoURL);
+  db.on("connected", () =>
+    console.log(`${mongoURL}정상적으로 연결되었습니다.`)
+  );
 
   db.on("error", (error) =>
-    console.error(
-      "MongoDB 연결에 실패하였습니다.\n" + ATLAS_URL_CORRECT + "\n" + error
-    )
+    console.error(`MongoDB 연결에 실패하였습니다.\n ${mongoURL}\n${error}`)
   );
 
   db.on("disconnected", () => {
-    console.log("mongoDB 연결이 끊겼습니다. 연결을 재시도 합니다.");
-    connect(ATLAS_URL_CORRECT);
+    console.log(`mongoDB 연결이 끊겼습니다. 연결을 재시도 합니다.`);
+    connect(mongoURL);
   });
 }
 
--- back/index.js
+++ back/index.js
@@ -2,17 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import db from "./configs/dbConfig";
-import cors from "cors";
 import { shelterRouter } from "./routers/shelterRouter";
-const userRouter = require("./routers/userRouter")
+const userRouter = require("./routers/userRouter");
 
 dotenv.config();
 db();
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 5000;
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use(shelterRouter);
