5f24b857ef23edde05df9ba6dbaff5dcd26f087b
[AI8/1반] 박영현 <chanel0908@gmail.com>
2023-08-22 18:31:58 +0900

    Feat: Review 관련 기능 코드 초안 추가

--- back/index.js
+++ back/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import db from "./configs/dbConfig";
 import cors from "cors";
 import { shelterRouter } from "./routers/shelterRouter";
+import { reviewRouter } from "./routers/reviewRouter";
 const userRouter = require("./routers/userRouter")
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use(shelterRouter);
 app.use(userRouter);
+app.use(reviewRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
