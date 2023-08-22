0b0c3fb2122e40fdc6c8643befd22cf4d31b2607
[AI8/1반] 김준섭 <elice.kdt@gmail.com>
2023-08-22 19:05:20 +0900

    현재 위치 취득 및 상태관리 관련 리팩토링

--- front/src/pages/PoiPage.js
+++ front/src/pages/PoiPage.js
@@ -4,51 +4,9 @@ import { useNavigate } from "react-router-dom";
 import PoiMap from "../components/Poi/PoiMap";
 import PoiList from "../components/Poi/PoiList";
 import DistrictSelector from "../components/Poi/DistrictSelector";
+import CurrentLocation from "../components/Poi/CurrentLocation";
 import PoiDataContext from "../contexts/PoiDataContext";
 import * as Api from "../apis/api";
-
-const CurrentLocation = () => {
-
-  const [isActive, setisActive] = useState(false);
-  
-  // Geolocation API를 사용해서 현재 위치 좌표를 가져오는데 필요한 변수, 옵션, 콜백함수를 정의합니다.
-  let currentLat = 0;
-  let currentLng = 0;
-
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  }
-  
-  function success(position) {
-    currentLat = position.coords.latitude;
-    currentLng = position.coords.longitude;
-    alert(`위도: ${position.coords.latitude} /` + ` 경도: ${position.coords.longitude} /` + ` 정확도: 약 ${position.coords.accuracy} 미터`);
-    // [TO-DO] 
-    // 마커에 현재 위치를 표시해주는 함수();
-    // map.setView([currentLat, currentLng]);
-  }
-  
-  function error(err) {                         // when getCurrentPosition() fails
-    const errMsg = ["zero-filler", "PERMISSION_DENIED", "POSITION_UNAVAILABLE", "TIMEOUT"];
-    alert("현재 위치를 가져올 수 없습니다.\nERROR: " + errMsg[err.code]);
-  }
-  
-  function getCurrentPosition() {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
-    if(isActive){
-      
-    }
-  }
-  
-  return (
-    <div>
-      <button id="current-position-btn" className="px-3 py-1 bg-green-400 rounded-xl" onClick={getCurrentPosition}>현재 위치</button>
-    </div>
-  )
-}
 
 // [참고사항] Leaflet.js의 Map 크기는 상위 div의 크기에 따라 상대적으로 결정되며, Tailwind CSS와 호환되지 않습니다.
 //          /src/index.css 파일에서 일반 CSS 코드를 추가하여 크기를 지정해주어야 합니다.
@@ -57,11 +15,20 @@ const PoiPage = () => {
 
   const navigate = useNavigate();
 
+  // 자식 컴포넌트인 CurrentLocation 컴포넌트로부터 오는 현재 위치 좌표를 상태값으로 사용합니다.
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  function handleLatLngState(currentLatitude, currentLongitude) {
+    setLatitude(currentLatitude);
+    setLongitude(currentLongitude);
+  }
+      
   // 사용자가 선택한 지역은 자식 컴포넌트인 DistrictSelector를 통해서 처리됩니다.
   // 자식 컴포넌트인 DistrictSelector가 부모 컴포넌트인 PoiPage의 district 상태값을 변경시킬 수 있도록 state handler를 사용합니다.
   // DistrictSelector.js를 참고하세요.
   const [district, setDistrict] = useState("");
-  function handleState(selectedDistrict) {
+  function handleDistrictState(selectedDistrict) {
     setDistrict(selectedDistrict)
   }
 
@@ -99,6 +66,7 @@ const PoiPage = () => {
         })
 
       } catch (err) {
+        // 만약에 에러가 발생하게 되면 데이터 로딩 상황을 알려주는 placeholder 아래에 에러 메세지가 추가됩니다.
         setError(`${err.name} : ${err.message}`);
         console.log(error);
         alert(`데이터를 가져오는 도중 에러가 발생했습니다: ${err}`);
@@ -118,13 +86,16 @@ const PoiPage = () => {
     );
   }
 
+
+  
+
   return (
     <PoiDataContext.Provider value={districtPoiData}>
       <div id="poi-page-wrapper" className="flex flex-col overflow-y-auto">
         <div id="poi-toolbar-wrapper" className="flex-none">
           <div id="poi-toolbar" className="flex flex-row justify-between items-center h-8 px-8 mb-3">
-            <DistrictSelector handleState={handleState} /> <span>{district}</span>
-            <CurrentLocation />
+            <DistrictSelector handleState={handleDistrictState} /> <span>{district}</span>
+            <CurrentLocation handleState={handleLatLngState} />
           </div>
         </div>
         <div id="poi-content-wrapper" className="grow overflow-y-auto flex flex-row">
@@ -132,7 +103,7 @@ const PoiPage = () => {
               <PoiList />
           </div>
           <div id="poi-map" className="flex-1">
-              <PoiMap />
+              <PoiMap lat={latitude} lon={longitude} />
           </div>
         </div>  
       </div>
