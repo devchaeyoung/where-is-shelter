9554c11ddf0d4d2ee959d003f800209b51c15ca1
[AI8/1반] 김준섭 <elice.kdt@gmail.com>
2023-08-24 12:10:06 +0900

    front 브랜치에서 back 폴더 완전히 제외

--- back/db/index.js
+++
@@ -1,5 +0,0 @@
-import { UserModel } from "./models/userModel";
-import { ShelterModel } from "./models/shelterModel";
-import { ReviewModel } from "./models/reviewModel";
-
-export { UserModel, ShelterModel, ReviewModel };
