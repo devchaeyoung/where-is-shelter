26ea459096e4d91d46c4e9b60ba67abb058379ef
[AI8/1반] 김준섭 <elice.kdt@gmail.com>
2023-08-24 21:41:37 +0900

    custom hooks 기반으로 API 호출 리팩토링, 현재 위치 주기적으로 받아오는 기능 추가

---
+++ front/src/hooks/useApi.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+const useApi = (fetcher) => {
+
+  // 백엔드로부터 받아온 데이터가 탑재되는 상태값입니다
+  const [data, setData] = useState(null);
+  
+  // 백엔드로부터 데이터를 받아오고 있는지를 체크하는 상태값입니다.
+  const [isFetching, setIsFetching] = useState();
+
+  // 백엔드로부터 데이터를 받아오다가 오류가 발생했는지를 체크하는 상태값입니다.
+  const [error, setError] = useState();
+  
+  useEffect(() => {
+
+    setIsFetching(true);
+    setError(false);
+    
+    try{   
+      const data = fetcher();
+      setData(data);
+
+    } catch (err) {
+      setError(error);
+    }
+
+    setIsFetching(false);
+  }, [])
+
+  return {
+    data,
+    isFetching,
+    error
+  }
+}
+
+export default useApi;
