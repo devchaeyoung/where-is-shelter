fc37660a5bb0c8657884bbe9919ad185f178b4cc
[AI8/1반] 박영현 <chanel0908@gmail.com>
2023-08-25 22:34:28 +0900

    Fix: console.log 삭제 및 코드 최적화

--- back/controllers/reviewController.js
+++ back/controllers/reviewController.js
@@ -8,30 +8,31 @@ class ReviewController {
         const result = await ReviewService.getReviewByUserId({ user_id });
         res.status(StatusCodes.OK).json(result);
       } catch (e) {
-        console.log(e);
         next(e);
       }
     }
 
     static async getReviewByShelterId(req, res, next) {
       try {
+        console.log(req.params);
+        console.log(req.body)
         const { shelter_id } = req.params;
         const result = await ReviewService.getReviewByShelterId({ shelter_id });
         res.status(StatusCodes.OK).json(result);
       } catch (e) {
-        console.log(e);
         next(e);
       }
     }
 
     static async addReview(req, res, next) {
       try {
+        console.log(req.parmas);
+        console.log(req.body);
         const { shelter_id } = req.params;
         const { description, rating, user_id } = req.body;
         const result = await ReviewService.addReview({ description, rating, user_id, shelter_id })
         res.status(StatusCodes.CREATED).json(result)
       } catch (e) {
-        console.log(e);
         next(e);
       }  
     }
@@ -43,7 +44,6 @@ class ReviewController {
         const result = await ReviewService.setReview({ review_id, toUpdate: { description, rating } });
         res.status(StatusCodes.OK).json(result)
       } catch (e) {
-        console.log(e);
         next(e);
       }
     }
@@ -54,7 +54,6 @@ class ReviewController {
         const result = await ReviewService.deleteReview({ review_id });
         res.status(StatusCodes.NO_CONTENT).json(result)
       } catch (e) {
-        console.log(e);
         next(e);
       }
     }
--- back/db/models/reviewModel.js
+++ back/db/models/reviewModel.js
@@ -2,32 +2,27 @@ import { Review } from "../schemas/review";
 
 class ReviewModel {
   static async create(newReview) {
-    const createdNewReview = await Review.create(newReview);
-    return createdNewReview;
+    return await Review.create(newReview);
   }
   
   // 마이페이지에서 유저가 작성한 리뷰를 가져옴
   static async findByUserId(user_id) {
-    const user_reviews = await Review.find({ user_id });
-    return user_reviews;
+    return await Review.find({ user_id });
   }
 
   // 쉼터 상세 페이지에서 쉼터에 대한 리뷰를 가져옴
   static async findByShelterId(shelter_id) {
-    const shelter_reviews = await Review.find({ shelter_id });
-    return shelter_reviews;
+    return await Review.find({ shelter_id });
   }
 
   // 유저가 특정 쉼터에 작성한 리뷰를 가져옴
   static async findByUserIdShelterId(user_id, shelter_id) {
-    const user_shelter_review = await Review.findOne({ user_id, shelter_id });
-    return user_shelter_review;
+    return await Review.findOne({ user_id, shelter_id });
   }
   
   // 특정 리뷰 가져옴
   static async findById(review_id) {
-    const review = await Review.findOne({ _id: review_id });
-    return review;
+    return await Review.findOne({ _id: review_id });
   }
 
   // 리뷰 수정
@@ -36,19 +31,17 @@ class ReviewModel {
     const update = { $set: updateField };
     const option = { returnOriginal: false };
 
-    const updatedReview = await Review.findOneAndUpdate(
-        filter,
-        update,
-        option
+    return await Review.findOneAndUpdate(
+      filter,
+      update,
+      option
     );
-    return updatedReview;
   }
 
   // 리뷰 삭제
   static async findByIdAndRemove(review_id) {
     const filter = { _id: review_id };
-    const deletedReview = await Review.findOneAndDelete(filter);
-    return deletedReview;
+    return await Review.findOneAndDelete(filter);
   }
 }
 
