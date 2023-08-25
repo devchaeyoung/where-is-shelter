6c008a2548dc209d0db5def09cf0ae78ffc6ed38
[AI8/1반] 진채영 <chaemaa00@gmail.com>
2023-08-25 23:48:41 +0900

    style :MyPage 전체 컴포넌트 변경

--- front/src/components/MyPage/UserFavorite.js
+++
@@ -1,7 +0,0 @@
-function UserFavorite() {
-  return (
-    <div className="border-blue-400 border-2">UserFavorite컴포넌트입니다.</div>
-  );
-}
-
-export default UserFavorite;
--- front/src/pages/MyPage.js
+++ front/src/pages/MyPage.js
@@ -1,25 +1,50 @@
-import { React } from "react";
-import User from "../components/MyPage/User";
-import UserReviews from "../components/MyPage/UserReviews";
-import UserFavorite from "../components/MyPage/UserFavorite";
-import LoginForm from "../components/MyPage/LoginForm";
+import { React, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import UserProfile from "../components/MyPage/UserProfile";
+
+import * as Api from "../apis/api";
 
 function MyPage() {
-  const user = "로그인";
+  const navigate = useNavigate();
+
+  // API 요청에 사용되는 endpoint를 지정해줍니다.
+  const endpoint = "/user/mypage";
+
+  const getUserInfo = async () => {
+    try {
+      const res = await Api.getData(endpoint);
+      console.log(res);
+    } catch (e) {
+      alert(e.response.data);
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
   return (
-    <>
-      {user === "로그인" ? (
-        <div className="grow overflow-y-auto flex flex-row">
-          <User />
-          <div className="scroll-smooth overflow-y-scroll">
-            <UserFavorite />
-            <UserReviews />
-          </div>
+    <div className="flex flex-row overflow-y-auto min-h-full p-8 justify-between">
+      <div className="flex flex-col bg-slate-100 w-4/12 p-8 items-center h-96">
+        <UserProfile />
+        <div className="flex justify-between w-full">
+          <p className="text-xl font-bold mt-4">홍길동</p>
+          <p className="text-l font-bold mt-4">수정</p>
         </div>
-      ) : (
-        <LoginForm />
-      )}
-    </>
+        <div className="flex flex-col w-full">
+          <p className="text-m font-semi mt-4">리뷰작성 횟수별 등급(임시)</p>
+          <p className="text-m font-semi mt-4">user 한줄소개란입니다.</p>
+        </div>
+      </div>
+      <div className="flex flex-col w-7/12">
+        <div className="flex bg-slate-100 w-full h-48 p-8">
+          <p className="text-xl font-bold">내가 즐겨 찾은 쉼터</p>
+        </div>
+        <div className="flex bg-slate-100 w-full h-full mt-10 p-8">
+          <p className="text-xl font-bold">내가 남긴 쉼터 후기</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
