1821976b2478b66bf04466119bec5efb0335dde5
[AI8/1반] 박지호 <wlghwk0509@gmail.com>
2023-08-30 20:53:09 +0900

    fix: 소셜로그인 기능 구현

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,4 +1,5 @@
 import UserService from "../services/userService";
+import axios from "axios";
 import { StatusCodes } from "http-status-codes";
 
 class UserController {
@@ -114,30 +115,30 @@ class UserController {
   static async kakaoLogin(req, res, next) {
     const code = req.query.code
     try {
-      const accessTokenGet = await Axios.post('https://kauth.kakao.com/oauth/token', {}, {
+      const kakaoKey = process.env.KAKAO_RESTAPIKEY;
+      const accessTokenGet = await axios.post('https://kauth.kakao.com/oauth/token', {}, {
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         params: {
           grant_type: 'authorization_code',
-          client_id: process.env.KAKAO.RESTAPIKEY,
+          client_id: kakaoKey,
           code,
-          redirect_uri: 'localhost:5000/user/auth/kakao'
+          redirect_uri: 'http://localhost:5000/user/auth/kakao'
         }
       })
 
-      const getKakaoUserInfo = await Axios.post('https://kapi.kakao.com/v2/user/me', {}, {
+      const getKakaoUserInfo = await axios.post('https://kapi.kakao.com/v2/user/me', {}, {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
           'Authorization': 'Bearer ' + accessTokenGet.data.access_token
         }
       })
 
-      const KakaoUserInfo = getKakaoUserInfo.data
+      const kakaoUserInfo = getKakaoUserInfo.data
       const user = await UserService.getAuthUser({ 
-        email: KakaoUserInfo.email, 
-        nickname: KakaoUserInfo.profile.nickname, 
-        name: KakaoUserInfo.name 
+        email: kakaoUserInfo.kakao_account.email, 
+        nickname: kakaoUserInfo.kakao_account.profile.nickname, 
       })
-
+      
       res.status(200).send(user)
     } catch (error) {
       next(error)
