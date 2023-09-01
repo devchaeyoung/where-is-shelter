2b049b7246af407e93804738d331e697e18b901b
[AI8/1반] 박지호 <wlghwk0509@gmail.com>
2023-09-02 01:56:42 +0900

    back 롤백

--- back/middlewares/profileMiddleware.js
+++ back/middlewares/profileMiddleware.js
@@ -4,18 +4,16 @@ import path from "path";
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, cb) {
-      console.log(req);
-      console.log("-----hello world");
       cb(null, "uploads/");
     },
     filename(req, file, cb) {
       /**확정자 이름*/
       const ext = path.extname(file.originalname);
-      console.log("profile image profile image");
+      console.log(ext);
       cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
     },
   }),
-  limits: { fieldSize: 2 * 1024 * 1024 },
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 export default upload;
