9d81d99d55317650192b478aec70f41f06ab8722
[AI8/1반] 진채영 <chaemaa00@gmail.com>
2023-09-01 18:46:02 +0900

    style: 인포그래픽 변경

--- front/src/pages/InfographicPage.js
+++ front/src/pages/InfographicPage.js
@@ -3,6 +3,7 @@ import { React, useState, useEffect, useContext } from 'react';
 import ChartItemSelector from "../components/Infographic/ChartItemSelector";
 import Graph from "../components/Infographic/Graph";
 import Commentary from "../components/Infographic/Commentary";
+import elice from "../assets/staticDB/elice.png"
 
 const InfographicPage = () => {
  
@@ -33,11 +34,38 @@ const InfographicPage = () => {
               <Commentary metadata={currentChartData.metadata}/>
             </div>
           </div>
-        : <div className="flex flex-row w-11/12 h-full items-center justify-center p-10 m-10 bg-slate-100 rounded-xl">
-            <img className="w-[20%]" src="images/infographic-placeholder.svg" 
+        : <div className="flex flex-col w-11/12 h-full items-center justify-center p-10 m-10 bg-slate-100 rounded-xl">
+            {/* <img className="w-[20%]" src="images/infographic-placeholder.svg" 
                                      alt="인포그래픽 메뉴의 대기 화면입니다."
-                                     style={{opacity: 0.5, filter: 'grayscale(1)'}}/>
-            <h1 className="ml-12 font-bold text-xl text-slate-500">화면에 표시할 통계 항목을 선택해주세요.</h1>
+                                     style={{opacity: 0.5, filter: 'grayscale(1)'}}/> */}
+            <img src={elice} art="엘리스 토끼" style={{width:"16rem", height: "15rem"}}/>
+            <h1 className="ml-8 mt-6 font-bold text-xl text-slate-500">화면에 표시할 통계 항목을 선택해주세요.</h1>
+            <div className="flex items-center mt-16">
+            <div className="">
+            <div className="flex flex-col justify-center bg-white mr-4 p-10 shadow-md border border-dotted border-teal-600 rounded-2xl">
+            <a href ="https://www.weather.go.kr/w/index.do">
+                <img className="w-28" alt="기상청 로고"
+                    // style={{backgroundColor: 'white'}}
+                    src="https://www.kma.go.kr/kma/resources/images/sub/sig5.png"/></a>
+            </div>
+        </div>
+        <div className="">
+            <div className="flex flex-col justify-center bg-white mr-4 p-10 shadow-md border border-dotted border-teal-600 rounded-2xl">
+            <a href="https://www.mois.go.kr/frt/a01/frtMain.do">
+                <img className="w-28" alt="행정안전부 로고"
+                    src="https://www.mois.go.kr/frt2022/main/img/common/logo4.png"/></a>
+            </div>
+        </div>
+        <div className="">
+            <div className="flex flex-col justify-center bg-white p-10 shadow-md border border-dotted border-teal-600 rounded-2xl">
+            <a href="https://www.safekorea.go.kr/idsiSFK/neo/main/main.html">
+                <img className="w-28 h-8" alt="국민재난안전포털 로고"
+                     style={{backgroundColor: 'white'}}
+                    src="https://www.safekorea.go.kr/idsiSFK/neo/ext/img/main/main_logo_img.png"/></a>
+            </div>
+        </div>
+              </div>
+              
           </div>
       }
     </div>
