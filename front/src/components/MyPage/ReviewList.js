e8de475963009825109ac3bbe00da0b1f361098c
[AI8/1반] 진채영 <chaemaa00@gmail.com>
2023-09-02 03:18:30 +0900

    style: 유저쉼터후기 쉼터명

--- front/src/components/MyPage/ReviewList.js
+++ front/src/components/MyPage/ReviewList.js
@@ -3,7 +3,7 @@ const ReviewList = props => {
     <div className="w-full h-full">
       {props.list.map(item => (
         <div key={item.shelter_id} className="flex flex-col my-2">
-          <p className="">{item.shelter.name}</p>
+          <p className="font-bold">{item.shelter.name}</p>
           <p className="">{item.description}</p>
         </div>
       ))}
